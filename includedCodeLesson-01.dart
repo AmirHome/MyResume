75966b66cd3720ffca5d9af6889fe01b99e315c6
amirhome <amir.email@yahoo.com>
2022-07-25 11:05:41 +0300

    DartPad append

---
+++ includedCodeLesson-01.dart
@@ -0,0 +1,48 @@
+"/* Time 10 */
+ class MyVariable {
+   /* Type */
+   bool myBool = true;
+   int myInt = -45;
+   double myDouble = -9999.88;
+   String myString = 'String';
+   List myArray = [1, 2, 2, 2, 3, 4];
+   List<String> myArrayStr = ['1,2,2,3', '4'];
+   Set mySet = <String>{'A', 'B', 'C'}; // unique
+   Map<String, dynamic> myMap = {
+     'A': 1,
+     'B': 'two',
+     'C': 3,
+     'D': {'D1': 1, 'D2': 2}
+   }; // json
+
+   var myVar = 'var';
+   dynamic myDynamic = 'Dynamic';
+
+   final String myFinal = 'Final';
+   static const double myConst = 3.14;
+
+   void myChanges() {
+     /* Wrong */
+     //myFinal = 'Final Change';
+     //myConst = 'const Change';
+     /* Dont Change Type */
+     myVar = 'var changed';
+     myDynamic = 'Dynamic changed' + ' ' + myString;
+   }
+
+   void showDebug() {
+     /* Debug */
+     print(myBool);
+     print(myInt);
+     print(myDouble);
+     print(myString);
+     print(myArray);
+     print(myArrayStr);
+     print(mySet);
+     print(myMap);
+     print(myVar);
+     print(myDynamic);
+     print(myFinal);
+     print(myConst);
+   }
+ }"
